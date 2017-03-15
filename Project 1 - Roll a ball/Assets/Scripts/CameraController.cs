e5cce9bf9d9580e84b8f18505aea3d7d15fbc84d
zakstacks <zakariyah@gmail.com>
2017-03-16 00:48:39 +0200

    Roll a ball
    Still incomplete

---
+++ Project 1 - Roll a ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;     //want to keep the camera a fixed distance from the player
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;    //the offset is the initial distance to the player
+	}
+	
+	// Update is called once per frame, after all updates have been processed
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Project 1 - Roll a ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rigidBody;
+    public float speed;
+
+    private void Start()    //called on first frame of this script
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()  //called before physics calculations
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rigidBody.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up")) {
+            other.gameObject.SetActive(false);
+        }
+        Destroy(other.gameObject);  //all components, children and their components are destroyed
+    }
+}
